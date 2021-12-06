a79e4c2d0e777b98cc530607a3d7c4bd138eb0a9
2042577 William Velasco <william.velasco@correounivalle.edu.co>
2021-12-06 08:38:42 -0500

    Finished

--- src/juegoCraps/GUIGridBagLayout.java
+++ src/juegoCraps/GUIGridBagLayout.java
@@ -29,6 +29,8 @@ public class GUIGridBagLayout extends JFrame {
         initGUI();
         //Default JFrame configuration
         this.setTitle("Juego Craps");
+        this.setUndecorated(true);
+        this.setBackground(new Color(255,255,255,0));
         this.pack();
         this.setResizable(true);
         this.setVisible(true);
@@ -52,7 +54,7 @@ public class GUIGridBagLayout extends JFrame {
         constraints.fill = GridBagConstraints.HORIZONTAL;
         this.add(headerProject,constraints);
 
-        ayuda = new JButton(" ? "); // puedo poner icono o imgen
+        ayuda = new JButton(" ? ");
         ayuda.addActionListener(escucha);
         constraints.gridx = 0;
         constraints.gridy = 1;
@@ -62,13 +64,65 @@ public class GUIGridBagLayout extends JFrame {
         this.add(ayuda,constraints);
 
         salir = new JButton("Salir");
-        ayuda.addActionListener(escucha);
+        salir.addActionListener(escucha);
         constraints.gridx = 1;
         constraints.gridy = 1;
         constraints.gridwidth = 1;
         constraints.fill = GridBagConstraints.NONE;
         constraints.anchor = GridBagConstraints.LINE_END;
         this.add(salir,constraints);
+
+        imageDado = new ImageIcon(getClass().getResource("/resources/dado.jpg"));
+        dado1 = new JLabel(imageDado);
+        dado2 = new JLabel(imageDado);
+
+        panelDados = new JPanel();
+        panelDados.setPreferredSize(new Dimension(300,180));
+        panelDados.setBorder(BorderFactory.createTitledBorder("Tus Dados"));
+        panelDados.add(dado1);
+        panelDados.add(dado2);
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 1;
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.anchor = GridBagConstraints.CENTER;
+
+        add(panelDados,constraints);
+
+        resultadosDados = new JTextArea(4,31);
+        resultadosDados.setBorder(BorderFactory.createTitledBorder("Resultados "));
+        resultadosDados.setText("Debes lanzar los dados");
+        resultadosDados.setBackground(null);
+        resultadosDados.setEditable(false);
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        constraints.gridwidth = 1;
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.anchor = GridBagConstraints.CENTER;
+        add(resultadosDados,constraints);
+
+        lanzar = new JButton("lanzar");
+        lanzar.addActionListener(escucha);
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        constraints.gridwidth = 2;
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.anchor = GridBagConstraints.CENTER;
+        add(lanzar,constraints);
+
+        mensajesSalida = new JTextArea(4,31);
+        mensajesSalida.setText("Usa el botón (?) para ver las reglas del juego");
+        mensajesSalida.setBorder(BorderFactory.createTitledBorder("Mensajes "));
+        mensajesSalida.setBackground(null);
+        mensajesSalida.setEditable(false);
+        JScrollPane scroll = new JScrollPane(mensajesSalida);
+        constraints.gridx = 0;
+        constraints.gridy = 4;
+        constraints.gridwidth = 2;
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.anchor = GridBagConstraints.CENTER;
+        add(mensajesSalida,constraints);
     }
 
     public static void main(String[] args){
@@ -81,17 +135,24 @@ public class GUIGridBagLayout extends JFrame {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            modelCraps.calcularTiro();
-            int[] caras = modelCraps.getCaras();
-            imageDado = new ImageIcon(getClass().getResource("/resources/"+caras[0]+".png"));
-            dado1.setIcon(imageDado);;
-            imageDado = new ImageIcon(getClass().getResource("/resources/"+caras[1]+".png"));
-            dado2.setIcon(imageDado);
-            modelCraps.determinarJuego();
-
-            resultadosDados.setText(modelCraps.getEstadoToString()[0]);
-            mensajesSalida.setRows(4);
-            mensajesSalida.setText(modelCraps.getEstadoToString()[1]);
+            if(e.getSource() == lanzar){
+                modelCraps.calcularTiro();
+                int[] caras = modelCraps.getCaras();
+                imageDado = new ImageIcon(getClass().getResource("/resources/"+caras[0]+".png"));
+                dado1.setIcon(imageDado);;
+                imageDado = new ImageIcon(getClass().getResource("/resources/"+caras[1]+".png"));
+                dado2.setIcon(imageDado);
+                modelCraps.determinarJuego();
+                resultadosDados.setText(modelCraps.getEstadoToString()[0]);
+                mensajesSalida.setRows(4);
+                mensajesSalida.setText(modelCraps.getEstadoToString()[1]);
+            }else{
+                if(e.getSource() == ayuda){
+                    JOptionPane.showMessageDialog(null,MENSAJE_INICIO);
+                }else{
+                    System.exit(0);
+                }
+            }
         }
     }
 }
